489af7161d43bed5eb2fedd411e1153561fb872c
Logan Ballard <loganballard@gmail.com>
2022-01-06 00:13:00 -0500

    additions deletions

--- dist/index.js
+++ dist/index.js
@@ -8449,10 +8449,18 @@ try {
   // skip if not PR
   if (github.context.action !== "pull_request") {
       console.log("not a PR")
+      return
   }
+  const pr_payload = github.context.payload.pull_request
 
+  let added = JSON.parse(pr_payload.body).additions
+  let subbed = JSON.parse(pr_payload.body).deletions
 
   console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  console.log(`additions: ${added}`);
+  console.log(`deleted: ${subbed}`);
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
