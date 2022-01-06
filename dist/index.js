19be90d1d326535c81332599a281d1a5dd264d8d
Logan Ballard <loganballard@gmail.com>
2022-01-05 23:58:10 -0500

    rebuild w ncc

--- dist/index.js
+++ dist/index.js
@@ -8446,11 +8446,6 @@ const core = __nccwpck_require__(7115);
 const github = __nccwpck_require__(3077);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
