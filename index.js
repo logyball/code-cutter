583b5b98559547a5488b78935f943795650385cd
logan <33067676+logyball@users.noreply.github.com>
2022-01-06 08:12:16 -0500

    init Pr (#1)
    * additions deletions
    
    * add pr
    
    * context
    
    * stringify
    
    * eventName
    
    * stringify
    
    * add and sub
    
    * no parse
    
    * token
    
    * token
    
    * input
    
    * comment
    
    * int
    
    * token
    
    * secret
    
    * pr object
    
    * promise
    
    * new pr
    
    * adding test stuff and desc

--- index.js
+++ index.js
@@ -3,12 +3,35 @@ const github = require('@actions/github');
 
 try {
   // skip if not PR
-  if (github.context.action !== "pull_request") {
+  if (github.context.eventName !== "pull_request") {
       console.log("not a PR")
+      return
   }
 
+  if (github.context.action !== "opened") {
+    console.log("PR is not new")
+    return
+  }
+  const pr_payload = github.context.payload.pull_request;
+  const token = core.getInput('github-token');
+  let added = parseInt(pr_payload.additions);
+  let deleted = parseInt(pr_payload.deletions);
 
-  console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+  if (token == "") {
+    throw new Error("no gh token");
+  }
+
+  const octokit = github.getOctokit(token)
+
+  if (added < deleted) {
+    octokit.rest.issues.createComment({
+        issue_number: pr_payload.number,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        body: "🎉 🏆 This PR deletes more code than it adds! 🎉 🏆"
+    })
+  }
+
 } catch (error) {
   core.setFailed(error.message);
 }
